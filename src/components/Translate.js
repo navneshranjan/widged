fe514e21bbdb28735e282e35af1ddea91aff5b1f
navneshranjan <navneshranjan@gmail.com>
2021-03-18 21:57:33 +0530

    Building the convert component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,7 @@
+// const KEY = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
 import React, { useState } from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 const options = [
   {
     label: "Afrikans",
@@ -31,6 +33,9 @@ const Translate = () => {
         onSelectedChange={setLanguage}
         options={options}
       />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert text={text} language={language} />
     </div>
   );
 };
